864370009b6e2710ad5a1aed64bb20595c89b4a8
titof <hktitof@gmail.com>
2022-07-21 08:12:25 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-yellow-800">Hello Word! 1 test</div>;
+  return <div className="text-yellow-800">Key = {process.env.NEXT_PUBLIC_KEY}</div>;
 }
