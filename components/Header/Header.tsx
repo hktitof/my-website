2d1f0d8e3b7b60fdfb3619e477dd9a565aa72884
titof <hktitof@gmail.com>
2022-07-22 08:53:15 +0100

    navbar hide when scroll down and show when scroll up

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,23 +1,38 @@
 import React, { useRef, useState, useEffect } from "react";
+const addClass=(ref:any,myclass:string)=>{
+  ref.current?.classLIst.add(myclass);
+}
 const Header = () => {
-  const RefNavBar = useRef();
+  const RefNavBar = useRef<HTMLDivElement>(null);
   let scrollSizeY = 0;
+  //Hide when scroll down & show when scroll up
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", e => {
-      scrollSizeY = window.scrollY;
-      if (scrollSizeY > 24) {
-        RefNavBar.current?.classList.add("bg-white");
+      if(scrollSizeY==0){
+        scrollSizeY = window.scrollY;
+      }else{
+        if(window.scrollY>50){
+
+          if (window.scrollY> scrollSizeY ) {
+            if(RefNavBar){
+              RefNavBar.current?.classList.remove("translate-y-0");
+              RefNavBar.current?.classList.add("-translate-y-full")
+            }
+          }else{
+            RefNavBar.current?.classList.add("translate-y-0");
+              RefNavBar.current?.classList.remove("-translate-y-full")
+          }
+          scrollSizeY=window.scrollY;
+        }
       }
-      console.log(scrollSizeY);
+      console.log("Windows is scrolling scrollY = ",scrollSizeY);
     });
   }
-  // transition -translate-y-full duration-30
-
   return (
     <div
       ref={RefNavBar}
       className="w-full fixed bg-opacity-75  flex 
-      justify-between px-12 py-4 drop-shadow-lg "
+      justify-between px-12 py-4 drop-shadow-lg transition duration-4000 translate-y-0"
     >
       <span className="text-AAsecondary">Logo</span>
       <div className="font-mono text-xs flex flex-row items-center space-x-8">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,14 @@
 import Header from "../components/Header/Header";
 export default function Home() {
-  //check if it's in the browser
-  
-  // scrollbar-thin
-  //       scrollbar-thumb-gray-600 scrollbar-track-AAprimary 
-  //        overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full
-
   return (
-
-      <div
-        className="relative h-screen bg-AAprimary w-full pt-4 "
-      >
-        <Header />
-        <span className="text-white">Hello</span>
-        <div className="h-96  w-full"></div>
-        <div className="h-96 bg-red-300 w-full"></div>
-        <div className="h-96 bg-blue-300 w-full"></div>
-        <div className="h-96 bg-yellow-500 w-full"></div>
-        <div className="h-96 w-full"></div>
-      </div>
+    <div className="relative h-screen bg-AAprimary w-full pt-4 ">
+      <Header />
+      <span className="text-white">Hello</span>
+      <div className="h-96  w-full"></div>
+      <div className="h-96 bg-red-300 w-full"></div>
+      <div className="h-96 bg-blue-300 w-full"></div>
+      <div className="h-96 bg-yellow-500 w-full"></div>
+      <div className="h-96 w-full"></div>
+    </div>
   );
 }
