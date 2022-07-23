d49294ffe971bf728b0aa5dbd45cb7b3d0718381
titof <hktitof@gmail.com>
2022-07-23 19:56:09 +0100

    fix mobile pinned Logo after moving to the Menu

--- components/Header/StartupLogo/Startup.tsx
+++ components/Header/StartupLogo/Startup.tsx
@@ -3,13 +3,15 @@ import { motion } from "../../../node_modules/framer-motion/dist/framer-motion";
 const Startup = () => {
   let WidthBy2 = 0;
   let HeightBy2 = 0;
+  let greaterThanSmall=false;
   if (typeof window !== "undefined") {
     if (window.innerWidth > 768) {
       WidthBy2 = window.innerWidth / 2 - 48 - 20;
       HeightBy2 = window.innerHeight / 2-44;
+      greaterThanSmall=true;
     }else{
-      WidthBy2 = window.innerWidth / 2 -30;
-      HeightBy2 = window.innerHeight / 2-44;
+      WidthBy2 = window.innerWidth / 2 -28;
+      HeightBy2 = window.innerHeight / 2-40;
     }
 
     console.log("width by 2: ", WidthBy2);
@@ -19,11 +21,11 @@ const Startup = () => {
     <motion.div 
     initial={{opacity:1}}
     animate={{opacity:0}}
-    transition={{opacity:{delay:5,duration:0}}}
+    transition={{opacity:{delay:4.9,duration:0}}}
     className="absolute h-full w-full flex justify-center items-center">
       <motion.div
         initial={{ opacity: 0, x: 0, y: 0, scale: "100%" }}
-        animate={{ opacity: [1, 0, 1], x: -WidthBy2, y:-HeightBy2,scale: "57%" }}
+        animate={{ opacity: [1, 0, 1], x: -WidthBy2, y:-HeightBy2,scale: greaterThanSmall?"57%":"50%" }}
         transition={{
           opacity: { delay: 3, duration: 1.5 },
           x: { duration: 0.5, delay: 4.5 },
