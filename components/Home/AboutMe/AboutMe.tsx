6e76fd699db5f7b6e4dbe312db9934dc5d17d627
titof <hktitof@gmail.com>
2022-07-28 09:59:54 +0100

    About me is set up

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -19,28 +19,28 @@ const ArrowIcon = () => {
 export default function AboutMe() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center py-24 ">
+      <div className="flex flex-col justify-center items-center py-24 bg-AAprimary">
         {/* 0.1 About Me */}
         <div
           className="flex flex-col space-y-8 px-9 sm:px-0 w-full sm:w-[500px] 
-        md:w-[700px] lg:w-[900px] bg-gray-800"
+        md:w-[700px] lg:w-[900px] "
         >
           <div className="flex items-center space-x-4 ">
             <div className="flex flex-row space-x-4 items-end ">
-              <span className="text-AAsecondary font-mono text-xl">
+              <span className="text-AAsecondary font-mono text-lg sm:text-xl">
                 &gt; 0.1
               </span>
               <span className="text-gray-300 font-mono text-xl sm:text-2xl">
                 About Me
               </span>
             </div>
-            <div className="bg-gray-400 h-[0.2px] w-32 sm:w-72"></div>
+            <div className="bg-gray-400 h-[0.2px] w-28 sm:w-72"></div>
           </div>
           {/* Paragraphs */}
 
-          <div className="w-full flex flex-col md:flex-row space-y-8 md:space-y-0">
+          <div className="w-full flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 sm:space-x-2">
             <div className="w-full md:w-7/12 space-y-4">
-              <div className="font-Header tracking-wide">
+              <div className="font-Header tracking-wider">
                 <span className="text-gray-400  ">
                   Hello! My name is Abdellatif and I enjoy solving problems and
                   creating codes that live on the internet. My interest in
@@ -52,7 +52,7 @@ export default function AboutMe() {
                   from that time!
                 </span>
               </div>
-              <div className="font-Header tracking-wide">
+              <div className="font-Header tracking-wider">
                 <span className="text-gray-400  ">
                   Fast-forward to today, and I&apos;ve had the privilege of
                   working at
@@ -61,7 +61,7 @@ export default function AboutMe() {
                   developing Smart Contracts. that time!
                 </span>
               </div>
-              <div className="font-Header tracking-wide">
+              <div className="font-Header tracking-wider">
                 <span className="text-gray-400  ">
                   I also recently Developed these Four projects that you&apos;ll
                   find below using Node & Next js.
@@ -111,9 +111,16 @@ export default function AboutMe() {
                 </div>
               </div>
             </div>
-            <div className="relative lg:w-96 lg:h-96 md:w-72 md:h-72 md:block hidden ">
-              <div className="absolute w-5/6 h-5/6 border-2 border-AAsecondary translate-x-5 translate-y-5 rounded"></div>
+            {/* Image in Desktop and Tablet */}
+            <div className="group relative lg:w-96 lg:h-96 md:w-72 md:h-72 md:block hidden ">
+              <div
+                className="group-hover:translate-x-3 group-hover:translate-y-3
+               duration-300 absolute w-5/6 h-5/6 border-2 border-AAsecondary translate-x-5 
+               translate-y-5 rounded"
+              ></div>
+
               <div className="absolute w-5/6 h-5/6  rounded overflow-hidden">
+                <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-60 duration-300 rounded overflow-hidden"></div>
                 {/* <div className="bg-scroll bg-[url('/img/titof.jpg')] h-full w-full fill"></div> */}
                 <img
                   className="object-contain rounded-lg"
@@ -122,10 +129,18 @@ export default function AboutMe() {
                 />
               </div>
             </div>
-            {/* <div className="relative w-full h-60 md:hidden bg-white flex justify-center items-center">
-              <div className="absolute w-64 h-full bg-blue-300 translate-x-4 translate-y-4 border-2 border-x-AAsecondary rounded"></div>
-              <div className="absolute w-64 h-full bg-red-300 rounded"></div>
-            </div> */}
+            {/* Image in Mobile */}
+            <div className=" relative w-full h-48 md:hidden  flex justify-center items-center">
+              <div className="absolute w-48 h-full  rounded  translate-x-5 translate-y-5 border-2 border-AAsecondary"></div>
+              <div className="absolute w-48 h-full rounded overflow-hidden">
+                <img
+                  className=" rounded-lg"
+                  src={"/img/titof.jpg"}
+                  alt="My Image Not Found"
+                />
+              </div>
+              <div className="absolute w-48 h-full  bg-AAsecondary opacity-60  rounded overflow-hidden"></div>
+            </div>
             {/* <div className="w-5/12">
               <div className="relative w-full border border-red-200 h-2/3">
                 <div className="relative bg-white w-10/12 h-full">
