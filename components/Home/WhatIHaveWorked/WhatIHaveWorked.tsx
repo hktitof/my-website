a85203b7a74a75ff0fa2844606c20c9a34a23da5
titof <hktitof@gmail.com>
2022-08-03 06:01:41 +0100

    WhatIHaveWorked's Title is created

---
+++ components/Home/WhatIHaveWorked/WhatIHaveWorked.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function WhatIHaveWorked() {
+  return (
+    <div className="flex justify-center py-24">
+        {/* Title "Where I've Worked" */}
+        <section className="flex flex-row space-x-3 items-center">
+            <span className="text-AAsecondary font-mono text-xl">&gt; 02.</span>
+            <span className="text-gray-200 text-2xl">What I&apos;ve Worked</span>
+            <div className="bg-gray-400 h-[0.2px] w-28 sm:w-80"></div>
+        </section>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import SocialMediaEmail from "../components/Home/SocialMediaArround/SocialMediaEmail";
 import AboutMe from "../components/Home/AboutMe/AboutMe";
 import ThisCantBeReached from "../components/Home/ThisSiteCantBeReached/ThisCantBeReached";
+import WhatIHaveWorked from "../components/Home/WhatIHaveWorked/WhatIHaveWorked";
 export default function Home() {
   const [ShowElement,setShowElement] = useState(false);
   const [ShowThisCantBeReached,setShowThisCantBeReached] = useState(true);
@@ -25,13 +26,17 @@ export default function Home() {
     },5400);
   },[])
   return (
-    <div className="relative h-screen bg-AAprimary w-full ">
-      {ShowThisCantBeReached?<ThisCantBeReached/>:<></>}
+    // ? h-screen is changed to be deleted 
+    // ? because it's making it fixed for the whole page
+    <div className="relative  bg-AAprimary w-full ">
+      {/* {ShowThisCantBeReached?<ThisCantBeReached/>:<></>}
       {ShowElement ? <Startup/>:<></>}
       <Header />
       <MyName />
-      <SocialMediaEmail />
-      {ShowAboutMe?<AboutMe />:<></>}
+      <SocialMediaEmail /> */}
+      {/* {ShowAboutMe?<AboutMe />:<></>} */}
+      <AboutMe />
+      <WhatIHaveWorked />
     </div>
   );
 }
