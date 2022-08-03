60863f85fd4f5a44aaed9ebfe4c3b485fe6b34b5
titof <hktitof@gmail.com>
2022-08-03 10:40:47 +0100

    comment is added

--- components/Home/WhatIHaveWorked/WhatIHaveWorked.tsx
+++ components/Home/WhatIHaveWorked/WhatIHaveWorked.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 export default function WhatIHaveWorked() {
-  const barRef=React.useRef<HTMLDivElement>(null);
-  const barPositions:string[]=["translate-y-0","translate-y-8"];
-  let barCurrentPosition="translate-y-0"
-  const barTranslateY=(position:number)=>{
+  const barRef = React.useRef<HTMLDivElement>(null);
+  const barPositions: string[] = ["translate-y-0", "translate-y-8"];
+  let barCurrentPosition = "translate-y-0";
+  // ! FIXME: Bar animation needs to be fixed when clicking multiple times to change the position
+  const barTranslateY = (position: number) => {
     barRef.current?.classList.remove(barCurrentPosition);
     barRef.current?.classList.add(barPositions[position]);
-    barCurrentPosition=barPositions[position];
-  }
+    barCurrentPosition = barPositions[position];
+  };
   return (
     <div className="flex flex-col items-center justify-center py-24 space-y-12">
       {/* Title "Where I've Worked" */}
@@ -24,19 +25,28 @@ export default function WhatIHaveWorked() {
           {/* left bar Holder */}
           <div className="relative h-96 w-0.5 bg-gray-500 rounded">
             {/* animated left bar */}
-            <div ref={barRef} className={`absolute w-0.5 h-10 rounded bg-AAsecondary
-            transition ${barCurrentPosition} duration-300`}></div>
+            <div
+              ref={barRef}
+              className={`absolute w-0.5 h-10 rounded bg-AAsecondary
+            transition ${barCurrentPosition} duration-300`}
+            ></div>
           </div>
           {/* Companies name as buttons */}
           <div className="flex flex-col">
-            <button onClick={()=>barTranslateY(0)} className="text-sm text-left hover:text-AAsecondary
+            <button
+              onClick={() => barTranslateY(0)}
+              className="text-sm text-left hover:text-AAsecondary
              hover:bg-ResumeButtonHover rounded text-gray-400 font-mono  
-             py-2 pl-6 px-4 w-44">
+             py-2 pl-6 px-4 w-44"
+            >
               Google
             </button>
-            <button onClick={()=>barTranslateY(1)} className="text-sm text-left hover:text-AAsecondary
+            <button
+              onClick={() => barTranslateY(1)}
+              className="text-sm text-left hover:text-AAsecondary
              hover:bg-ResumeButtonHover rounded text-gray-400 font-mono
-              py-2 pl-6 px-4 w-44">
+              py-2 pl-6 px-4 w-44"
+            >
               Facebook
             </button>
           </div>
