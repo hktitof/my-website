c25c6a8cda3d9942cd97216b0a9c7bf7294c2fe7
titof <hktitof@gmail.com>
2022-08-23 17:25:59 +0100

    adding table to UserData project

--- pages/userData/index.tsx
+++ pages/userData/index.tsx
@@ -20,8 +20,8 @@ export default function Page() {
   const userData = useRef<any>(null);
   let windowWidth = useRef<HTMLSpanElement>(null);
   let windowHeight = useRef<HTMLSpanElement>(null);
-  let mouseX=useRef<HTMLSpanElement>(null);
-  let mouseY=useRef<HTMLSpanElement>(null);
+  let mouseX = useRef<HTMLSpanElement>(null);
+  let mouseY = useRef<HTMLSpanElement>(null);
   // ? Window size Tracker
   useEffect(() => {
     // Apply this event Listener on Client
@@ -129,17 +129,27 @@ export default function Page() {
     // }
   };
   console.log("user data : ", userData.current);
-  const BlockElem = (props)=>{
-    return(
+  const BlockElem = props => {
+    return (
       <div className="flex flex-row ">
-      <span className={`text-gray-200 font-semibold text-lg flex-none ${props.size}`}>
-        {props.title}
-      </span>
-      <span className="text-AAsecondary font-semibold text-lg">
-        {props.value}
-      </span>
-    </div>
-    )
+        <span
+          className={`text-gray-200 font-semibold text-lg flex-none ${props.size}`}
+        >
+          {props.title}
+        </span>
+        <span className="text-AAsecondary font-semibold text-lg">
+          {props.value}
+        </span>
+      </div>
+    );
+  };
+  const TableRow = props => {
+  return(
+    <tr className="border-2 border-gray-300">
+    <td className="border-2 border-gray-300 pl-4 py-3">{props.title}</td>
+    <td className="pl-4 text-AAsecondary">{props.value}</td>
+  </tr>
+  );
   }
   return (
     <>
@@ -151,108 +161,191 @@ export default function Page() {
       </Head>
       <div className=" w-full bg-AAprimary text-white pt-44 2xl:px-64 xl:px-44 lg:px-24 md:px-16 px-4 ">
         <div className="h-full w-full bg-AAtertiary py-16 sm:px-12 px-4">
-          <div className="h-full w-full bg-gray-800 flex md:flex-row flex-col ">
+          <div className="h-full w-full  flex md:flex-row flex-col ">
             {/* // ? User Data */}
-            <div className="h-full md:w-2/3 md:order-1 order-2 border-2 pr-10 flex flex-col space-y-3 ">
-              <div className="pb-2">
-                <span className="text-2xl font-bold">
+            <div className="h-full md:w-2/3 md:order-1 order-2  md:pr-10 flex flex-col space-y-3 ">
+              <div className="pb-2 sm:pt-0 pt-4">
+                <span className="text-xl sm:text-2xl font-bold pb-8">
                   General Information :
                 </span>
               </div>
-              <BlockElem size="w-32" title="IP Address" value={userData.current?.query || "Checking..."} />
-              <BlockElem size="w-32" title="Browser" value={userData.current?.browser || "Checking..."} />
-              <BlockElem size="w-32" title="City :" value={userData.current?.city || "Checking..."} />
-              <BlockElem size="w-32" title="Zip Code :" value={userData.current?.zip || "Checking..."} />
-              <BlockElem size="w-32" title="Region :" value={userData.current?.regionName || "Checking..."} />
-              <BlockElem size="w-32" title=" Region Code :" value={userData.current?.region || "Checking..."} />
-              <BlockElem size="w-32" title="Country :" value={userData.current?.country || "Checking..."} />
-              <BlockElem size="w-44" title="Current Date/time :" value={userData.current?.datetime || "Checking..."} />
-              <BlockElem size="w-32" title="Timezone :" value={userData.current?.timezone || "Checking..."} />
-              <BlockElem size="w-32" title="As :" value={userData.current?.as || "Checking..."} />
-              {/* // ? Additional Information */}
-              <div className="pb-2">
-                <span className="text-2xl font-bold">
+              <table className="border-2 border-gray-300 w-full">
+                <tbody>
+                <TableRow
+                size="w-32"
+                title="IP Address"
+                value={userData.current?.query || "Checking..."}
+              />
+
+              <TableRow
+                size="w-32"
+                title="City :"
+                value={userData.current?.city || "Checking..."}
+              />
+              <TableRow
+                size="w-32"
+                title="Zip Code :"
+                value={userData.current?.zip || "Checking..."}
+              />
+              <TableRow
+                size="w-32"
+                title="Region :"
+                value={userData.current?.regionName || "Checking..."}
+              />
+              <TableRow
+                size="w-32"
+                title=" Region Code :"
+                value={userData.current?.region || "Checking..."}
+              />
+              <TableRow
+                size="w-32"
+                title="Country :"
+                value={userData.current?.country || "Checking..."}
+              />
+              <TableRow
+                size="w-44"
+                title="Current Date/time :"
+                value={userData.current?.datetime || "Checking..."}
+              />
+              <TableRow
+                size="w-32"
+                title="Timezone :"
+                value={userData.current?.timezone || "Checking..."}
+              />
+              <TableRow
+                size="w-32"
+                title="As :"
+                value={userData.current?.as || "Checking..."}
+              />
+                </tbody>
+              </table>
+              
+              <div className="pb-2 sm:pt-0 pt-4">
+                <span className="text-xl sm:text-2xl font-bold">
                   Additional Information :
                 </span>
               </div>
-              <BlockElem size="w-32" title="Browser :" value={userData.current?.browser || "Checking..."} />
-              <BlockElem size="w-32" title="version :" value={userData.current?.browserVersion || "Checking..."} />
-              <div className="flex flex-row ">
-                <span className="text-gray-200 font-semibold text-lg flex-none w-32">
-                  Languages:
-                </span>
-                <span className="text-AAsecondary font-semibold text-lg">
-                  {userData.current?.NavigatorLanguages.toString().replace(
-                    ",",
-                    ", "
-                  ) || "Checking..."}
-                </span>
-              </div>
-              <BlockElem size="w-32" title="OS :" value={userData.current?.browserOS || "Checking..."} />
-              <BlockElem size="w-32" title="CPU cores :" value={userData.current?.NavigatorLogicalCores || "Checking..."} />
-              <BlockElem size="w-32" title=" Screen orientation :" value={userData.current?.screenOrientationType || ""} />
-              <BlockElem size="w-32" title="Screen size :" value={(userData.current?.screenWidth || "") +
-                    " x " +
-                    (userData.current?.screenHeight || "")} />
-              
-              
-              <div className="flex flex-row ">
-                <span className="text-gray-200 font-semibold text-lg flex-none w-32">
-                  Window size :
-                </span>
-                <span className="text-AAsecondary font-semibold text-lg">
-                  <span ref={windowWidth} className="text-AAsecondary">
-                    {userData.current?.screenWidth || ""}
-                  </span>
-                  <span className="text-gray-300"> x </span>
-                  <span ref={windowHeight} className="text-AAsecondary">
-                    {userData.current?.screenHeight || ""}
-                  </span>
-                </span>
-              </div>
+              <section className="flex flex-col lg:flex-row lg:space-y-0 space-y-3 lg:space-x-4">
+                {/* // ? Additional Information Section 1*/}
+                <div className="flex flex-col space-y-3">
+                  <BlockElem
+                    size="w-32"
+                    title="Browser :"
+                    value={userData.current?.browser || "Checking..."}
+                  />
+                  <BlockElem
+                    size="w-32"
+                    title="version :"
+                    value={userData.current?.browserVersion || "Checking..."}
+                  />
+                  <div className="flex flex-row ">
+                    <span className="text-gray-200 font-semibold text-lg flex-none w-32">
+                      Languages:
+                    </span>
+                    <span className="text-AAsecondary font-semibold text-lg">
+                      {userData.current?.NavigatorLanguages.toString().replace(
+                        ",",
+                        ", "
+                      ) || "Checking..."}
+                    </span>
+                  </div>
+                  <BlockElem
+                    size="w-32"
+                    title="OS :"
+                    value={userData.current?.browserOS || "Checking..."}
+                  />
+                  <BlockElem
+                    size="w-32"
+                    title="CPU cores :"
+                    value={
+                      userData.current?.NavigatorLogicalCores || "Checking..."
+                    }
+                  />
+                </div>
 
-              <div className="flex flex-row ">
-                <span className="text-gray-200 font-semibold text-lg flex-none w-36">
-                  Mouse position :
-                </span>
-                <span className="text-AAsecondary font-semibold text-lg">
-                <span className="text-gray-300">X - </span>
-                  <span ref={mouseX} className="text-AAsecondary">
-                    {0}
-                  </span>
-                  <span className="text-gray-300">, Y - </span>
-                  <span ref={mouseY} className="text-AAsecondary">
-                    {0}
-                  </span>
-                </span>
-              </div>
-              <BlockElem size="w-44" title="Screen Color Depth :" value={userData.current?.screenColorDepth || "Checking..."} />
+                {/* // ? Additional Information Section 2 */}
+                <div className="flex flex-col space-y-3">
+                  <BlockElem
+                    size="w-44"
+                    title=" Screen orientation :"
+                    value={userData.current?.screenOrientationType || ""}
+                  />
+                  <BlockElem
+                    size="w-32"
+                    title="Screen size :"
+                    value={
+                      (userData.current?.screenWidth || "") +
+                      " x " +
+                      (userData.current?.screenHeight || "")
+                    }
+                  />
+
+                  <div className="flex flex-row ">
+                    <span className="text-gray-200 font-semibold text-lg flex-none w-32">
+                      Window size :
+                    </span>
+                    <span className="text-AAsecondary font-semibold text-lg">
+                      <span ref={windowWidth} className="text-AAsecondary">
+                        {userData.current?.screenWidth || ""}
+                      </span>
+                      <span className="text-gray-300"> x </span>
+                      <span ref={windowHeight} className="text-AAsecondary">
+                        {userData.current?.screenHeight || ""}
+                      </span>
+                    </span>
+                  </div>
+
+                  <div className="flex flex-row ">
+                    <span className="text-gray-200 font-semibold text-lg flex-none w-36">
+                      Mouse position :
+                    </span>
+                    <span className="text-AAsecondary font-semibold text-lg">
+                      <span className="text-gray-300">X - </span>
+                      <span ref={mouseX} className="text-AAsecondary">
+                        {0}
+                      </span>
+                      <span className="text-gray-300">, Y - </span>
+                      <span ref={mouseY} className="text-AAsecondary">
+                        {0}
+                      </span>
+                    </span>
+                  </div>
+                  <BlockElem
+                    size="w-44"
+                    title="Screen Color Depth :"
+                    value={userData.current?.screenColorDepth || "Checking..."}
+                  />
+                </div>
+              </section>
             </div>
             {/* // ? Map  */}
-            <div className="h-full w-full md:w-1/3 bg-gray-800 flex flex-col space-y-8 items-center md:order-2 order-1">
+            <div className="h-full w-full md:w-1/3 flex flex-col space-y-8 items-center md:order-2 order-1 md:pt-12">
               <div className="relative md:h-96 h-64 w-full">
                 <div
                   className={`${
                     updatingLocation ? "" : "hidden"
                   } absolute h-full w-full border-[1px] border-white z-10 flex justify-center items-center`}
                 >
-                  <svg
-                    aria-hidden="true"
-                    className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600
+                  <div className="flex flex-col space-y-2 items-center">
+                    <svg
+                      aria-hidden="true"
+                      className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600
                      fill-AAsecondary"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
-                  </svg>
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="">Updating location...</span>
+                  </div>
                 </div>
                 <div
                   className={`${
