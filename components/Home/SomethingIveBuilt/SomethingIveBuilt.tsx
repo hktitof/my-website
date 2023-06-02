b603ee93316a30cbf8f728e5741511aa57fd7afa
titof <hktitof@gmail.com>
2023-06-03 00:29:16 +0100

    added ens vision

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -11,7 +11,7 @@ export default function SomethingIveBuilt() {
   return (
     <div
       id="SomethingIveBuiltSection"
-      className=" flex flex-col space-y-12 bg-AAprimary w-full  
+      className=" flex flex-col xl:space-y-28 space-y-12 bg-AAprimary w-full  
      2xl:px-72 lg:px-24 md:px-16 sm:px-16 py-32 px-4"
     >
       {/* // ? Title  */}
@@ -27,8 +27,86 @@ export default function SomethingIveBuilt() {
         <div className="bg-gray-400 h-[0.2px] w-full xl:w-1/3 md:w-1/2"></div>
       </div>
 
-      <div className="flex flex-col space-y-8 md:space-y-28">
-        {/* // ?  Project 1 */}
+      <div className="flex flex-col   xl:space-y-36 space-y-8 md:space-y-28">
+        {/* // ?  Project  1*/}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              {/* <Link href={"/typing"}>
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAsecondary 
+           transition-opacity opacity-20 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </Link> */}
+
+              <a href="https://www.ens.vision" target={"_blank"} rel="noreferrer">
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-30 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <Img src={"/ensvision.jpg"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/ensvision.jpg"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">ENS Vision </span>
+                <a href="https://ens.vision" target="_blank" rel="noopener noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    ENS names Marketplace
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right ">
+                  At <span className="text-AAsecondary">ENS Vision</span>, I played a crucial role in developing web3
+                  components for their <span className="text-AAsecondary">web3 application</span> using Next.js and web3 libraries. Our focus was on integrating
+                  new features and improving the user interface. One notable achievement was the release of NameWrapper
+                  with <span className="text-AAsecondary">ERC72</span> support, enabling users to create subdomains.
+                  This showcased our commitment to delivering innovative solutions.
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">ENS domain</span>
+                <span className="pr-4 z-10">ENS names</span>
+                <span className="pr-4 z-10">web3</span>
+                <span className="pr-4 z-10">NFT</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <a href="https://www.ens.vision/" target={"_blank"} rel="noreferrer">
+                  <ExternalLink url={""} router={router} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* // ?  Project 2 */}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
           {/* Left image */}
           <div
@@ -98,7 +176,7 @@ export default function SomethingIveBuilt() {
             </div>
           </div>
         </div>
-        {/* // ?  Project  2*/}
+        {/* // ?  Project  3*/}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
           {/* Left image */}
           <div
@@ -110,7 +188,7 @@ export default function SomethingIveBuilt() {
                 <div
                   // onClick={}
                   className="absolute w-full h-full rounded bg-AAsecondary 
-           transition-opacity opacity-20 hover:opacity-0 hover:cursor-pointer duration-300"
+           transition-opacity opacity-10 hover:opacity-0 hover:cursor-pointer duration-300"
                 ></div>
               </Link>
 
@@ -166,7 +244,7 @@ export default function SomethingIveBuilt() {
           </div>
         </div>
 
-        {/* // ?  Project 3 */}
+        {/* // ?  Project 4 */}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
           {/* Left image */}
           <div
@@ -177,7 +255,7 @@ export default function SomethingIveBuilt() {
               <Link href={"/userdatapuller"}>
                 <div
                   className="absolute w-full h-full rounded bg-AAsecondary 
-           transition-opacity opacity-20 hover:opacity-0 hover:cursor-pointer duration-300"
+           transition-opacity opacity-10 hover:opacity-0 hover:cursor-pointer duration-300"
                 ></div>
               </Link>
               <Img src={"/hackme.jpg"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
@@ -235,7 +313,7 @@ export default function SomethingIveBuilt() {
           </div>
         </div>
 
-        {/* // ? Project 4 */}
+        {/* // ? Project 5 */}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96">
           {/* Left image */}
           <div
@@ -312,7 +390,7 @@ export default function SomethingIveBuilt() {
           </div>
         </div>
 
-        {/* // ?  Project 5 */}
+        {/* // ?  Project 6 */}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
           {/* Left image */}
           <div
