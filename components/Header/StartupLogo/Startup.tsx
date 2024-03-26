0586d3f388ebc83c5a7b6034274be59c286da757
satyam04sharma <satyam04sharma@gmail.com>
2024-03-26 12:40:37 -0700

    Updated startup logo horizontal inclination

--- components/Header/StartupLogo/Startup.tsx
+++ components/Header/StartupLogo/Startup.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Spline from '@splinetool/react-spline';
+import styles from '../../../styles/Startup.module.css'; // Import the CSS module
+
 const Startup = (props) => {
 
   return (
-    <div className="h-full ">
+    <div className={`${styles.splineContainer}`}>
     <Spline scene="https://prod.spline.design/0E98HIpJnDUzvwzt/scene.splinecode" />
       </div>
   );
