f508e1b30c827f42c8c62ae51f91a8e408eedede
titof <hktitof@gmail.com>
2025-06-13 17:19:51 +0100

    updated seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -107,8 +107,8 @@ export default function Home() {
 
   console.log("website is rendering...");
   const meta = {
-    title: "Abdellatif Anaflous - Software Engineer",
-    description: `I've been working on Software development for 5 years straight. Get in touch with me to know more.`,
+    title: "Abdellatif Anaflous - Lead Full Stack Software Engineer",
+    description: `I've been working on Software development for more than 7 years. Get in touch with me to know more.`,
     image: "/titofCercle.png",
     type: "website",
   };
